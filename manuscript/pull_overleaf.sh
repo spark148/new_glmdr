7ef3e84af59356bd104ba047cff2559d1511e0a5
spark148 <spark148@illinois.edu>
2020-09-21 09:56:45 -0500

    GitHub Connecting Test

--- manuscript/pull_overleaf.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-## This script pulls the latex from overleaf.
-## It may requires the credentials
-
-git clone https://git.overleaf.com/5f57eef045d91700015607e9
-rm -rf Overleaf
-mv 5f57eef045d91700015607e9 Overleaf
